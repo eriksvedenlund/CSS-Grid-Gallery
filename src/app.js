a8324e3204e71b73c70b05fa3a2a5d5f7f169472
lenalast <lenalast@gmail.com>
2017-11-29 15:16:40 +0100

    fetching photos from API

--- src/app.js
+++ src/app.js
@@ -5,7 +5,12 @@ import Grid from './components/grid';
 export default class App extends React.Component {
 
 	fetchPhotos = () => {
-		console.log('hej hej')
+		//Make get request
+
+		fetch('https://pixabay.com/api/?key=7218190-f36bf2d395f9f4e25477f5282&q=christmas&image_type=photo&per_page=6')
+			.then((res) => res.json())
+			.then( (res) => console.log(res))
+			.catch( (err) => console.log(err))
 	}
 
 	render() {
