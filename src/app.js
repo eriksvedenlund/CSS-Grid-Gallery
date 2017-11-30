6d23fdf9ecc6fab2376398ce3d51bc4bb7bc38d4
lenalast <lenalast@gmail.com>
2017-11-30 21:37:21 +0100

    Polyfill promise for IE

--- src/app.js
+++ src/app.js
@@ -1,63 +1,67 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import axios from 'axios';
+import 'core-js/fn/promise';
 import Grid from "./components/grid";
 import Header from "./components/Header";
 import "./sass/index.scss";
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      images: [],
-      searchQuery: ""
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			images: [],
+			searchQuery: ""
+		};
+	}
 
-  changeName = evt => {
-    this.setState({
-      searchQuery: evt.target.value
-    });
-    console.log(this.state.searchQuery);
-  };
+	changeName = evt => {
+		this.setState({
+			searchQuery: evt.target.value
+		});
+		console.log(this.state.searchQuery);
+	};
 
-  fetchImages = e => {
-    const url =
-      "https://pixabay.com/api/?key=7218190-f36bf2d395f9f4e25477f5282&q=" +
-      this.state.searchQuery +
-      "&image_type=photo&per_page=6";
+	fetchImages = e => {
+		const url =
+			"https://pixabay.com/api/?key=7218190-f36bf2d395f9f4e25477f5282&q=" +
+			this.state.searchQuery +
+			"&image_type=photo&per_page=6";
 
-    axios.get(url)
-      .then(res => {
-        this.setState({
-          images: res.data.hits
-        });
-      });
-  };
+		axios.get(url)
+			.then(res => {
+				this.setState({
+					images: res.data.hits
+				});
+			})
+		.catch(err => console.error(err))
+	};
 
-  componentDidMount() {
-    //Make get request
-    this.fetchImages();
-  }
-  componentDidUpdate() {
-    this.fetchImages();
-  }
-  render() {
-    let images = this.state.images.map((image, index) => {
-      const style = {
-        backgroundImage: `url('${image.webformatURL}')`
-      };
-      return <div key={index} className={"box" + index} style={style} />;
-    });
-    return (
-      <div>
-        <Header
-          changes={this.changeName.bind(this)}
-        />
-        <Grid images={images} />
-      </div>
-    );
-  }
+	componentDidMount() {
+		//Make get request
+		this.fetchImages();
+	}
+
+	componentDidUpdate() {
+		this.fetchImages();
+	}
+
+	render() {
+		let images = this.state.images.map((image, index) => {
+			const style = {
+				backgroundImage: `url('${image.webformatURL}')`
+			};
+			return <div key={index} className={"box" + index} style={style}/>;
+		});
+		return (
+			<div>
+				<Header
+					changes={this.changeName.bind(this)}
+				/>
+				<Grid images={images}/>
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
