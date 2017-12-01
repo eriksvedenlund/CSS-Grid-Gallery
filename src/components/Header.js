83b11db4ccff0ea6ec2062476b4dc88b37cf5499
enkasvenka <eriksvedenlund@hotmail.se>
2017-12-01 14:52:13 +0100

    link to github

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ const Header = props => (
   <header>
     <h1>CSS Grid Gallery</h1>
     <input type="text" onChange={props.changes} placeholder="Search for images"/>
+    <a href="https://github.com/eriksvedenlund/workflow-project/"><img src="https://cdn.pixabay.com/photo/2017/08/05/11/24/logo-2582757_1280.png" /></a>
   </header>
 );
 
